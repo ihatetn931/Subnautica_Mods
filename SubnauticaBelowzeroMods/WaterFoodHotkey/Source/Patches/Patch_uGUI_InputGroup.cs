5bb666c68db07f05714ffab21606ef8db3a32a31
ihatetn931 <shadowpot@gmail.com>
2021-06-06 15:26:08 -0500

    Removed uneeded stuff.

--- SubnauticaBelowzeroMods/WaterFoodHotkey/Source/Patches/Patch_uGUI_InputGroup.cs
+++ SubnauticaBelowzeroMods/WaterFoodHotkey/Source/Patches/Patch_uGUI_InputGroup.cs
@@ -11,25 +11,16 @@ namespace WaterFoodHotkeyBZ.Patches
         public static void Patch_uGUI_InputGroup_OnSelect()
         {
             MainPatch.EditNameCheck = true;
-#if DEBUG
-            Debug.Log($"[WaterDrinkHotkey] :: Patch_uGUI_InputGroup_OnSelect is {MainPatch.EditNameCheck}");
-#endif
         }
 
         public static void Patch_uGUI_InputGroup_OnDeselect()
         {
             MainPatch.EditNameCheck = false;
-#if DEBUG
-            Debug.Log($"[WaterDrinkHotkey] :: Patch_uGUI_InputGroup_OnDeselect is {MainPatch.EditNameCheck}");
-#endif
         }
 
         public static void Patch_uGUI_InputGroup_Deselect()
         {
             MainPatch.EditNameCheck = false;
-#if DEBUG
-            Debug.Log($"[WaterDrinkHotkey] :: Patch_uGUI_InputGroup_Deselect is {MainPatch.EditNameCheck}");
-#endif
         }
     }
 }
